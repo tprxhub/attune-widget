1ac17648fedf72d488113bbf75b329274204d35e
tprxhub <toyrxhub@thetoypharmacy.com>
2025-08-15 21:09:18 +0400

    Initial scaffold for Attune widget (React/Node).

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+// React chat UI scaffold for Attune
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Attune Chat UI</h1>
+        </div>
+    );
+};
+
+export default App;
